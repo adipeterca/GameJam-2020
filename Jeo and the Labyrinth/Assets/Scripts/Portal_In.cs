708e3ea78e75e8dfc003f0de723158d12b3b92eb
adipeterca <adipeterca@gmail.com>
2020-08-26 14:51:39 +0300

    Added some materials and a floor texture.

---
+++ Jeo and the Labyrinth/Assets/Scripts/Portal_In.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal_In : MonoBehaviour
+{
+    public Transform outPortal;
+    public float rotationSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 45f, 0) * rotationSpeed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = outPortal.position + new Vector3(0, 0.5f, 0);
+        }
+    }
+}
---
+++ Jeo and the Labyrinth/Assets/Scripts/Portal_Out.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal_Out : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime * rotationSpeed);
+    }
+}
