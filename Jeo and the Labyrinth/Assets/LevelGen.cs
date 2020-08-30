bc279e91acb27812c75f6e3a85721817da8d11ba
adipeterca <adipeterca@gmail.com>
2020-08-30 15:25:42 +0300

    Remade some prefabs and added WallTraps

---
+++ Jeo and the Labyrinth/Assets/LevelGen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Point
+{
+    int X, Y;
+    bool active;
+}
+
+public class LevelGen : MonoBehaviour
+{
+    public GameObject cube;
+    public int startX, startZ;
+    public int endX, endZ;
+
+    public Point[][] arrayPoints;
+
+
+    /*public int xStart, xEnd;
+    public int zStart, zEnd;
+
+    private void Start()
+    {
+        for (int i = xStart; i <= xEnd; i++)
+            for (int j = zStart; j <= zEnd; j++)
+                if (i % 5 == 0 && j % 5 == 0 && (i != 0 || j != 0))
+                    Instantiate(cube, new Vector3(i, 0, j), new Quaternion());
+    }*/
+}
---
+++ Jeo and the Labyrinth/Assets/WallTrap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTrap : MonoBehaviour
+{
+    public GameObject[] wall;
+
+    private void Start()
+    {
+        for (int i = 0; i < wall.Length; i++)
+            wall[i].SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            for (int i = 0; i < wall.Length; i++)
+                wall[i].SetActive(false);
+        }
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < wall.Length; i++)
+            {
+                wall[i].SetActive(true);
+                wall[i].GetComponent<Animator>().SetTrigger("Fade");
+            }
+        }
+    }
+}
